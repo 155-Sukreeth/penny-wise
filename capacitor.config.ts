67d802c4ceb096de729935f47de55e457dd89b29
Sukreeth Vandrangi <79745093+155-Sukreeth@users.noreply.github.com>
2026-09-07 23:55:30 +0530

    setup capacitor for android and update gitignore

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.pennywise.app',
+  appName: 'PennyWise',
+  webDir: 'dist'
+};
+
+export default config;
